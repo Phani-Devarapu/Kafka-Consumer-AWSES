d80aee96c9d3ee7b033a2417cab08b1f72b502f6
Phani Devarapu <phanidevarapu321@gmail.com>
2021-08-23 18:00:42 -0400

    Intial commit

---
+++ kafka-consumer-AWS-ES/src/main/java/com/adrinofast/kafkaconsumerAWSES/KafkaConsumerAwsEsApplication.java
@@ -0,0 +1,13 @@
+package com.adrinofast.kafkaconsumerAWSES;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaConsumerAwsEsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaConsumerAwsEsApplication.class, args);
+	}
+
+}
---
+++ kafka-consumer-AWS-ES/src/main/java/com/adrinofast/kafkaconsumerAWSES/config/Sample.java
@@ -0,0 +1,5 @@
+package com.adrinofast.kafkaconsumerAWSES.config;
+
+public class Sample {
+
+}
